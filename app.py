953be7dcbdd3f33942cc1c7a755302537a1fadf2
William Huynh <williamhuynh122@gmail.com>
2024-09-11 18:18:57 -0400

    Comm

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
 import datetime as date
 import scipy.stats as stats
 import streamlit as st
 import plotly.express as px
 import altair as alt
 
-vehicles = pd.read_csv(r"..\vehicles_us.csv")
-vehicles
+vehicles = pd.read_csv(r"vehicles_us.csv")
+st.dataframe(vehicles)
