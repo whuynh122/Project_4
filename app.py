89ca436caa4abeca55fee1ef52bf5e3f709e1706
William Huynh <williamhuynh122@gmail.com>
2024-09-16 20:12:23 -0400

    Comm

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ import altair as alt
 
 
 vehicles = pd.read_csv(r"vehicles_us.csv")
+median_year=vehicles.groupby(['model'])['model_year'].median()
+median_year=median_year.median().round()
+median_cylinders=vehicles.groupby(['model'])['cylinders'].median()
+median_cylinders=median_cylinders.median().round()
+mean_odometer=vehicles.groupby(['model','model_year'])['odometer'].mean()
+mean_odometer=mean_odometer.median().round()
+vehicles['model_year'] = vehicles['model_year'].fillna(median_year)
+vehicles['cylinders'] = vehicles['cylinders'].fillna(median_cylinders)
+vehicles['odometer'] = vehicles['odometer'].fillna(mean_odometer)
+vehicles['model_year'] = vehicles['model_year'].fillna(median_year)
+vehicles['cylinders'] = vehicles['cylinders'].fillna(median_cylinders)
+vehicles = vehicles[(vehicles['odometer'] <= vehicles['odometer'].quantile(0.95)) & (vehicles['odometer'] >= vehicles['odometer'].quantile(0.05)) &(vehicles['model_year']<= vehicles['model_year'].quantile(0.95))& (vehicles['model_year']>= vehicles['model_year'].quantile(0.05))] 
+
 
 st.set_page_config(layout="wide")
 st.header("Car advertisments Data:car:")
@@ -33,6 +46,7 @@ cols = st.columns(2)
 cols[0].plotly_chart(fig)
 
 cols[1].plotly_chart(scatter)
+
 
 
     
